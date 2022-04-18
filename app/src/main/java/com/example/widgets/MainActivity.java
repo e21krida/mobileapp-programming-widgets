b4578d81e8b0f620fed7a94bee34baabb16d0552
e21krida <e21krida@student.his.se>
2022-04-18 12:54:12 +0200

    added onClick containing logcat message on facebookButton & googleButton in MainActivity, fixed positioning errors

--- app/src/main/java/com/example/widgets/MainActivity.java
+++ app/src/main/java/com/example/widgets/MainActivity.java
@@ -4,13 +4,34 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button facebookButton;
+    private Button googleButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         Log.d("==>", "onCreate ran successfully");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        facebookButton = findViewById(R.id.facebook_button);
+        facebookButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("==>", "facebookButton pressed.");
+            }
+        });
+
+        googleButton = findViewById(R.id.google_button);
+        googleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("==>", "googleButton pressed.");
+            }
+        });
     }
 }
